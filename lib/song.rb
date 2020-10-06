a11c17b7fdae2124ad9b4ce0392d76d21255995e
Taylor Booth <taylorkbooth@gmail.com>
2020-10-06 16:12:19 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song 
+  
+    @@all = []
+  
+  attr_accessor :name
+  attr_reader :artist, :genre
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+        @@all.detect{|s| s.name == name}
+    end
+
+  def self.find_or_create_by_name(name)
+      if song = find_by_name(name) 
+         song
+      else
+         song = Song.create(name)
+      end
+    end
+    
+  def self.destroy_all
+    @@all = []
+  end
+  
+end
